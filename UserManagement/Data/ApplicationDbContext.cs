c2cc1c6f2c9d6ad36b96e8aac64831ebec0f45df
Sw1fT3rZ <130749643+Sw1fT3rZ@users.noreply.github.com>
2024-11-14 12:38:23 +0200

    Add files via upload

---
+++ UserManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementApp.Models;
+
+namespace UserManagementApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
