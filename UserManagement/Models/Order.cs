9474d4a2cae4df973c4e0b325159856035ab84e2
Sw1fT3rZ <daniil.shcherbak@nure.ua>
2024-11-21 20:15:10 +0200

    add orders

---
+++ UserManagement/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace UserManagementApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; } // Айди заказа
+        public string ProductName { get; set; } // Название продукта
+        public int Quantity { get; set; } // Количество продукта
+        public decimal Price { get; set; } //Цена заказа
+        public int UserId { get; set; } // Айдишник пользователя, сделавшего заказ
+        public User User { get; set; } // Пользователь, который сделал заказ
+    }
+}
